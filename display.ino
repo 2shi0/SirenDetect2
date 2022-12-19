137014cf7f8d24ae60e15672147e3cc17313d43b
Tsushima <b1019217@fun.ac.jp>
2022-12-19 11:42:54 +0900

    最終発表で使ったやつ

---
+++ display.ino
@@ -0,0 +1,196 @@
+// scaning... 文字
+String s = "Scaning";
+int c = 0;
+
+// 角度
+int arr = 0;
+
+void draw()
+{
+    sprite.setTextSize(2);
+
+    if (displayMode)
+    {
+        // 通常モード
+        nomalDisplay();
+
+        // スプライトを表示画面に転送する（描画）
+        sprite.pushSprite(0, 0);
+    }
+    else
+    {
+        // スプライトを黒く塗りつぶす
+        sprite.fillScreen(BLACK);
+        sprite.setCursor(0, 0);
+
+        // デバッグモード
+        debugDisplay();
+
+        // スプライトを表示画面に転送する（描画）
+        sprite.pushSprite(0, 0);
+    }
+}
+
+void nomalDisplay()
+{
+    sprite.setCursor(10, 10);
+    sprite.setTextSize(3);
+
+    if (detectFlag)
+    {
+        sprite.setTextColor(WHITE, RED);
+
+        // スプライトを塗りつぶす
+        sprite.fillRect(0, 0, M5.Lcd.width(), M5.Lcd.height(), RED);
+
+        sprite.println("DETECTED");
+
+        sprite.setTextSize(4);
+
+        if (activity > 0.6 && degree >= 0 && degree <= 360)
+        {
+            arr = arr * 0.75 + degree * 0.25;
+            // arr = 360 - degree;
+        }
+
+        double dis = arr;
+
+        // 矢印
+        sprite.setPivot(M5.Lcd.width() - M5.Lcd.height(), M5.Lcd.height() / 2);
+        arrow.pushRotated(&sprite, dis, BLUE);
+
+        // デバッグ用
+        sprite.setTextSize(2);
+        sprite.print("dis: ");
+        sprite.println(dis);
+        sprite.print("deg: ");
+        sprite.println(degree);
+    }
+    else
+    {
+        // スプライトを塗りつぶす
+        sprite.fillRect(0, 0, M5.Lcd.width(), M5.Lcd.height(), BLUE);
+
+        sprite.setTextColor(WHITE, BLUE);
+
+        c++;
+        if (c < 10)
+        {
+            sprite.println("Scaning");
+        }
+        else if (c < 20)
+        {
+            sprite.println("Scaning.");
+        }
+        else if (c < 30)
+        {
+            sprite.println("Scaning..");
+        }
+        else if (c < 40)
+        {
+            sprite.println("Scaning...");
+        }
+        else if (c < 50)
+        {
+            sprite.println("Scaning....");
+        }
+        else
+        {
+            sprite.println("Scaning....");
+            c = 0;
+        }
+
+        sprite.setTextColor(BLACK, BLUE);
+        sprite.setCursor(170, 100);
+        unsigned int b = (M5.Axp.GetBatVoltage() - 3) * 100 / 1.2;
+        sprite.print(b);
+        sprite.print("%");
+    }
+}
+void debugDisplay()
+{
+    sprite.setTextSize(2);
+
+    for (int i = 0; i < RMAX; i++)
+    {
+        // 人気バンドランキング
+        int hz = hzList[i];
+        sprite.setTextColor(WHITE);
+        if (i < 9)
+            sprite.print(" ");
+        sprite.print(i + 1);
+        sprite.print(":");
+        for (int j = 0; j < 5 - get_digit(hz); j++)
+        {
+            sprite.print(" ");
+        }
+        sprite.print(hz);
+        sprite.println(" Hz");
+    }
+
+    // 処理時間
+    sprite.setTextColor(DARKGREY);
+    sprite.print("    ");
+    sprite.print((float)(t2 - t1) / 1000000);
+    sprite.println(" ms");
+
+    // 現在のモード
+    sprite.setTextColor(YELLOW);
+
+    String modeText = "";
+    modeText += "1000Hz";
+    modeText += " Mode";
+    sprite.drawString(modeText, scrollCount, sprite.cursor_y);
+    sprite.println("");
+
+    // 文字スクロールのカウント
+    if (scrollCount > -(int(modeText.length()) * 12))
+    {
+        scrollCount -= 4;
+    }
+    else
+    {
+        scrollCount = 135;
+    }
+    // バッテリー残量
+    sprite.setTextColor(BLUE);
+    unsigned int b = (M5.Axp.GetBatVoltage() - 3) * 100 / 1.2;
+    sprite.print(b);
+    sprite.print("%");
+    sprite.print("(");
+    sprite.print(M5.Axp.GetBatVoltage());
+    sprite.print("V");
+    sprite.println(")");
+
+    if (M5.Axp.GetBatCurrent() > -1)
+    {
+        sprite.println("CHARGING");
+    }
+}
+
+// 桁数を求める
+unsigned int get_digit(unsigned int n)
+{
+    unsigned int digit = 1;
+
+    while (n /= 10)
+    {
+        ++digit;
+    };
+
+    return digit;
+}
+
+// 矢印画像　初期設定
+void arrowInit()
+{
+    arrow.createSprite(M5.Lcd.height(), M5.Lcd.height());
+
+    // 矢印BB
+    arrow.fillScreen(BLUE);
+    arrow.fillTriangle(M5.Lcd.height() / 2, 5,
+                       M5.Lcd.height() / 4, 50,
+                       M5.Lcd.height() * 3 / 4, 50,
+                       WHITE);
+    arrow.fillRect(M5.Lcd.height() * 2 / 5, 50, M5.Lcd.height() / 5, 80, WHITE);
+}
---
+++ lookBand.ino
@@ -0,0 +1,60 @@
+int deathCountGX = 10;
+
+bool firstCheck = 1;
+void lookBand()
+{
+    float GoldenRaspberry = 0;
+    for (int band = 10; band < 14; band++)
+    {
+
+        float stock = 0;
+        for (int i = 0; i < RMAX; i++)
+        {
+            stock += bandList[i][band];
+        }
+
+        /*
+        Serial.print(band);
+        Serial.print(" : ");
+        Serial.print((band * 1.0 * SAMPLING_FREQUENCY) / FFTsamples / 1000);
+        Serial.print("kHz : ");
+        Serial.println(stock);
+        */
+
+        GoldenRaspberry += stock;
+    }
+
+    //Serial.println(GoldenRaspberry);
+
+    if (GoldenRaspberry > 4000)
+    {
+        if (firstCheck)
+        {
+            viveFlag = 1;
+            firstCheck = 0;
+            ditectStock = 5;
+        }
+    }
+
+    if (GoldenRaspberry > 2000 && !firstCheck)
+    {
+        if (ditectStock < 5)
+            ditectStock++;
+    }
+    else
+    {
+        if (ditectStock > 0)
+
+            ditectStock--;
+    }
+
+    if (ditectStock > 2)
+    {
+        detectFlag = 1;
+    }
+    else if (ditectStock == 0)
+    {
+        detectFlag = 0;
+        firstCheck = 1;
+    }
+}
